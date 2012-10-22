c7e3bed0225428f30d68c5a7a85601ee81f83b4d
Fred Loney <loneyf@ohsu.edu>
2012-10-22 16:01:10 -0700

    Add package setup.

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 import glob
 from setuptools import setup, find_packages
 
+__version__ = open('version.txt').read()
+
+__doc__ = 'qipipe processes the OHSU QIN study images. See the README file for more information.'
 
 requires = ['pydicom']
 
 setup(
     name = 'qipipe',
-    version = '1.1.1',
+    version = __version__,
     author = 'Fred Loney',
     author_email = 'loneyf@ohsu.edu',
-    packages = find_packages('src'),
-    package_dir = {'':'src'},
+    packages = find_packages(),
     scripts = glob.glob('bin/*'),
     url = 'http://quip1.ohsu.edu/git/qipipe',
     license = 'Proprietary',
-    description = '.',
+    description = __doc__.split('.', 1)[0],
     long_description = __doc__,
     classifiers = [
         "Development Status :: 3 - Alpha",
