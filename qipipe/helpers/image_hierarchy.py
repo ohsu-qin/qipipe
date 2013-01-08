224945a58dd3ec593288b500ca8d011c89b355da
FredLoney <loneyf@ohsu.edu>
2013-01-08 13:11:50 -0800

    Use the select_dicom_tags method.

--- qipipe/helpers/image_hierarchy.py
+++ qipipe/helpers/image_hierarchy.py
@@ -1,8 +1,24 @@
 from collections import defaultdict
 from .dictionary_hierarchy import DictionaryHierarchy
-from .dicom_tag_reader import DicomTagReader
+from .dicom_reader import iter_dicom_headers, select_dicom_tags
+
+def read_image_hierarchy(*files):
+    """
+    Returns the ImageHierarchy for the DICOM files in the given locations.
+
+    @param files: the files or directories to walk for DICOM files
+    @return: the image hierarchy
+    @rtype: ImageHierarchy
+    """
+    # Build the hierarchy dictionary.
+    h = ImageHierarchy()
+    for ds in iter_dicom_headers(*files):
+        h.add(ds)
+    return h
 
 class ImageHierarchy(DictionaryHierarchy):
+    TAGS = ('PatientID', 'Study Instance UID', 'Series Instance UID', 'InstanceNumber')
+    
     """
     ImageHierarchy wraps the DICOM image patient-study-series-image hierarchy.
     """
@@ -10,7 +26,6 @@ class ImageHierarchy(DictionaryHierarchy):
         # the patient: series: image nested dictionary
         self.tree = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
         super(ImageHierarchy, self).__init__(self.tree)
-        self._tag_reader = DicomTagReader('PatientID', 'Study Instance UID', 'Series Instance UID', 'InstanceNumber')
     
     def add(self, ds):
         """
@@ -19,5 +34,5 @@ class ImageHierarchy(DictionaryHierarchy):
         @param ds: the DICOM dataset
         """
         # build the image hierarchy
-        path = self._tag_reader.read(ds)
+        path = select_dicom_tags(ds, *TAGS)
         self.tree[path[0]][path[1]][path[2]].append(path[3])
