ec86dc6f93f9fdb5cc46fb207e842cb587229100
FredLoney <loneyf@ohsu.edu>
2013-01-08 13:07:53 -0800

    Rename files to paths.

--- qipipe/helpers/file_helper.py
+++ qipipe/helpers/file_helper.py
@@ -2,23 +2,23 @@ import os
 
 class FileIterator(object):
     """
-    This FileIterator iterates over the files contained in one or more directories.
+    This FileIterator iterates over the paths contained in one or more directories.
     """
-    def __init__(self, *files):
+    def __init__(self, *paths):
         """
-        @param files: the file or directory paths.
+        @param paths: the file or directory paths.
         """
-        self._files = files
+        self._paths = paths
     
     def __iter__(self):
         return self.next()
         
     def next(self):
-        for f in self._files:
+        for f in self._paths:
             if os.path.isfile(f):
                 yield f
             elif os.path.isdir(f):
-                for root, dirs, files in os.walk(f):
-                    for f in files:
+                for root, dirs, paths in os.walk(f):
+                    for f in paths:
                         path = os.path.join(root, f)
                         yield path
