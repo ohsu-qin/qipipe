6499d27098d591adaef9f09f1a57679b3c84a2aa
FredLoney <loneyf@ohsu.edu>
2013-01-14 15:28:21 -0800

    Fix pydoc.

--- qipipe/helpers/collection_helper.py
+++ qipipe/helpers/collection_helper.py
@@ -2,7 +2,7 @@ from collections import Iterable
 
 def is_nonstring_collection(obj):
     """
-    @param node: the object to check
+    @param obj: the object to check
     @return: whether the given object is a non-string iterable object
     """
     return isinstance(obj, Iterable) and not isinstance(obj, str)
