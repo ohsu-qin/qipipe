c6fb749883f1afda49b63a58aefe2487c62a7dea
FredLoney <loneyf@ohsu.edu>
2013-01-15 11:47:08 -0800

    Import fix_dicom_headers.

--- qipipe/staging/__init__.py
+++ qipipe/staging/__init__.py
@@ -4,4 +4,5 @@ The staging package defines the functions used to prepare the study image files 
 submission to the TCIA QIN collections and pipeline processing.
 """
 from .link_dicom import link_dicom_files
+from .fix_dicom import fix_dicom_headers
 from .ctp import create_ctp_id_map
