1f6bec9df04e2717a347796080ed6bfcc9638f25
FredLoney <loneyf@ohsu.edu>
2013-01-18 16:54:14 -0800

    Move the singleton to a method attribute.

--- qipipe/staging/sarcoma_config.py
+++ qipipe/staging/sarcoma_config.py
@@ -1,9 +1,14 @@
 import os
-import ConfigParser
+import io
+from ConfigParser import ConfigParser as Config
 
 _CFG_FILE = os.path.join(os.path.dirname(__file__), '..', '..', 'conf', 'sarcoma.cfg')
-_CONFIG = ConfigParser()
-_CONFIG.read(_CFG_FILE)
 
 def sarcoma_location(pt_id):
-    return _CONFIG.get('Tumor Location', pt_id)
+    return sarcoma_config().get('Tumor Location', pt_id)
+
+def sarcoma_config():
+    if not hasattr(sarcoma_config, 'instance'):
+        sarcoma_config.instance = Config()
+        sarcoma_config.instance.read(_CFG_FILE)
+    return sarcoma_config.instance
