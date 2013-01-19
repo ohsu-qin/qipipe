8e8c7c9e1c4be33b3c10732579a7f6c43ea510a7
FredLoney <loneyf@ohsu.edu>
2013-01-18 17:01:41 -0800

    Move link to group.

--- qipipe/staging/__init__.py
+++ qipipe/staging/__init__.py
@@ -3,6 +3,6 @@
 The staging package defines the functions used to prepare the study image files for import into XNAT,
 submission to the TCIA QIN collections and pipeline processing.
 """
-from .link_dicom import group_dicom_files
+from .group_dicom import group_dicom_files
 from .fix_dicom import fix_dicom_headers
 from .ctp import create_ctp_id_map
