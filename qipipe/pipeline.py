413a725b906fef09ee2d695d8182aa25b47ce98c
FredLoney <loneyf@ohsu.edu>
2013-01-23 12:05:42 -0800

    Stack the series.

--- qipipe/pipeline.py
+++ qipipe/pipeline.py
@@ -1,8 +1,11 @@
 import nipype.pipeline.engine as pe
 from .interfaces.fix_dicom import FixDicom
+from nipype.interfaces.dcmstack import DcmStack
 from nipype.interfaces.fsl.preprocess import ApplyWarp
 
 
 pipeline = pe.Workflow(name='qipipe')
 fix_dicom = pe.Node(interface=FixDicom(), name='fix_dicom')
-pipeline.add_nodes([fix_dicom])
+stacker = pe.Node(interface=DcmStack(), name='stack')
+stacker.inputs.out_format = "series%(SeriesNumber)03d"
+pipeline.connect(fix_dicom, 'dest', stacker, 'dicom_files')
