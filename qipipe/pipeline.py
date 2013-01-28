effcfe9b80f310977a7b032dba07e6f43649cc0c
FredLoney <loneyf@ohsu.edu>
2013-01-28 09:49:36 -0800

    Add DcmStack step.

--- qipipe/pipeline.py
+++ qipipe/pipeline.py
@@ -1,11 +1,45 @@
 import nipype.pipeline.engine as pe
+from .interfaces.group_dicom import GroupDicom
 from .interfaces.fix_dicom import FixDicom
+from nipype.interfaces.io import DataGrabber
 from nipype.interfaces.dcmstack import DcmStack
 from nipype.interfaces.fsl.preprocess import ApplyWarp
 
 
+# The master pipeline.
 pipeline = pe.Workflow(name='qipipe')
-fix_dicom = pe.Node(interface=FixDicom(), name='fix_dicom')
-stacker = pe.Node(interface=DcmStack(), name='stack')
-stacker.inputs.out_format = "series%(SeriesNumber)03d"
-pipeline.connect(fix_dicom, 'dest', stacker, 'dicom_files')
+
+# Group the AIRC DICOM input files by series.
+group = pe.Node(interface=GroupDicom(), name='group', dest='data')
+
+# Fix the AIRC DICOM tags.
+fix = pe.Node(interface=FixDicom(), name='fix', dest='data')
+
+# TODO - in/pt/visit* +-> group/data/series* +-> fix/data -> stack/seriesnnnn.nii.gz -> datasink base_directory/(container = visitnn)
+# Use Mapper for in -> ( ... ) -> datasink ?
+
+# Grab the patient series subdirectories.
+p2v = pe.Node(interface=DataGrabber(), name='p2v')
+p2v.inputs.template = '*'
+p2v.inputs.field_template = dict(outfiles='visit*')
+
+# Grab the patient series subdirectories.
+v2s = pe.Node(interface=DataGrabber(), name='v2s')
+v2s.inputs.template = '*'
+v2s.inputs.field_template = dict(outfiles='series*')
+
+# Stack each series as a single NIfTI file.
+stack = pe.Node(interface=DcmStack(), name='stack')
+stack.inputs.out_format = "series%(SeriesNumber)03d"
+
+# Copy each series stack to the destination.
+assemble = pe.Node(nio.DataSink(), name='assemble')
+
+# Build the pipeline.
+pipeline.connect([
+    (group, fix, [('target', 'source')]),
+    (fix, p2v, [('target', 'base_directory')]),
+    (p2v, p2s, [('outfiles', 'base_directory')]),
+    (p2s, stack, [('outfiles', 'dicom_files')])
+    (stack, assemble [('out_file', '@')])
+    ])
--- test/unit/test_pipeline.py
+++ test/unit/test_pipeline.py
@@ -6,9 +6,7 @@ ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
 # The test fixture.
 FIXTURE = os.path.join(ROOT, 'fixtures', 'pipeline', 'sarcoma', 'patient03')
 # The test results parent directory.
-RESULTS = os.path.join(ROOT, 'results', 'pipeline')
-# The output patient directory.
-OUTPUT = os.path.join(ROOT, 'results', 'pipeline', 'sarcoma', 'patient03')
+RESULTS = os.path.join(ROOT, 'results', 'pipeline', 'sarcoma', 'patient03')
 
 from nipype import config
 cfg = dict(logging=dict(workflow_level='DEBUG', log_directory=RESULTS, log_to_file=True),
@@ -20,28 +18,23 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 from qipipe import pipeline
 from qipipe.helpers.dicom_helper import iter_dicom
 
-
-
-
-from nipype.interfaces.dcmstack import DcmStack
-
-
-
-
 class TestPipeline:
     """Pipeline unit tests."""
     
     def test_pipeline(self):
-        shutil.rmtree(OUTPUT, True)
-        pipeline.inputs.fix_dicom.source = FIXTURE
-        pipeline.inputs.fix_dicom.dest = OUTPUT
+        shutil.rmtree(RESULTS, True)
+        pipeline.inputs.group.source = FIXTURE
+        pipeline.inputs.group.dest = GROUPED
+        pipeline.inputs.fix.collection = 'Sarcoma'
+        pipeline.inputs.fix.dest = WORK
+        pipeline.inputs.assemble.base_directory = STACKED
         pipeline.run()
         # Verify the result.
         for ds in iter_dicom(OUTPUT):
             assert_equal('CHEST', ds.BodyPartExamined, "Incorrect Body Part: %s" % ds.BodyPartExamined)
             assert_equal('Sarcoma03', ds.PatientID, "Incorrect Patient ID: %s" % ds.PatientID)
         # Cleanup.
-        shutil.rmtree(RESULTS, True)
+        #shutil.rmtree(RESULTS, True)
 
 
 if __name__ == "__main__":
