3a7428723c66010dec1d246beb63be371428d3fe
FredLoney <loneyf@ohsu.edu>
2013-03-13 16:50:51 -0700

    Raise error if no match.

--- qipipe/staging/staging_helpers.py
+++ qipipe/staging/staging_helpers.py
@@ -1,11 +1,12 @@
 """Pipeline utility functions."""
 
 import re
+from .staging_error import StagingError
 
 _SSS_REGEX = '(\w+\d{2})/(session\d{2})/(series\d{3})'
 """The subject/session/series regexp pattern."""
 
-def match_session_hierarchy(path):
+def match_series_hierarchy(path):
     """
     Matches the subject, session and series names from the given input path.
     
@@ -15,3 +16,5 @@ def match_session_hierarchy(path):
     match = re.search(_SSS_REGEX, path)
     if match:
         return match.groups()
+    else:
+        raise StagingError("The path %s does match the subject/session/series pattern" % path)
