bd4fdafef072ba2e5247e70059fc9a97e40f1ac9
FredLoney <loneyf@ohsu.edu>
2013-03-22 16:26:10 -0700

    Support generators.

--- qipipe/helpers/file_helper.py
+++ qipipe/helpers/file_helper.py
@@ -1,24 +1,44 @@
 import os
+import inspect
+
+class FileIteratorError(Exception):
+    pass
 
 class FileIterator(object):
     """
-    This FileIterator iterates over the paths contained in one or more directories.
+    This FileIterator iterates over the files contained in the given file specifications.
     """
-    def __init__(self, *paths):
+    def __init__(self, *filespecs):
         """
-        @param paths: the file or directory paths.
+        @param filespecs: the files, directories or file generators over which to iterate
         """
-        self._paths = paths
+        self._filespecs = filespecs
     
     def __iter__(self):
         return self.next()
         
     def next(self):
-        for f in self._paths:
-            if os.path.isfile(f):
-                yield f
-            elif os.path.isdir(f):
-                for root, dirs, fnames in os.walk(f):
-                    for fn in fnames:
-                        path = os.path.join(root, fn)
-                        yield path
+        """
+        Yields the next file as follows:
+            - If the current file specification is a file, then yield that file.
+            - If the current file specification is a directory, then yield each file
+              contained in that directory.
+            - If the current file specification is a generator, then yield each
+              generated item 
+        """
+        for spec in self._filespecs:
+            if isinstance(spec, str):
+                if os.path.isfile(spec):
+                    yield spec
+                elif os.path.isdir(spec):
+                    for root, dirs, fnames in os.walk(spec):
+                        for fn in fnames:
+                            yield os.path.join(root, fn)
+                else:
+                    raise FileIteratorError("File not found: %s" % spec)
+            elif inspect.isgenerator(spec):
+                for f in spec:
+                    yield f
+            else:
+                raise FileIteratorError("File iteration item is not supported: %s" % spec.__class__)
+        
