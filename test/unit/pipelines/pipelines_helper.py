fe4c5ef46514fcfb9b0b9266f588cf73a87b03f4
FredLoney <loneyf@ohsu.edu>
2013-04-10 16:10:52 -0700

    Refactor test into helpers.

---
+++ test/unit/pipelines/pipelines_helper.py
@@ -0,0 +1,45 @@
+import os, re
+from qipipe.staging import airc_collection as airc
+from qipipe.staging.staging_helper import SUBJECT_FMT
+from qipipe.helpers.xnat_helper import XNAT
+
+import logging
+logger = logging.getLogger(__name__)
+
+def get_xnat_subjects(collection, source):
+    """
+    @param collection: the AIRC collection name
+    @param source: the input parent directory
+    @return: the XNAT subject => directory dictionary
+    """
+    
+    xnat = XNAT().interface
+    airc_coll = airc.collection_with_name(collection)
+    sbj_dir_dict = {}
+    for d in os.listdir(source):
+        match = re.match(airc_coll.subject_pattern, d)
+        if match:
+            # The XNAT subject label.
+            sbj_nm = SUBJECT_FMT % (collection, int(match.group(1)))
+            logger.debug("Checking whether the test subject %s exists in XNAT..." % sbj_nm)
+            # Delete the XNAT subject, if necessary.
+            sbj = xnat.select('/project/QIN/subject/' + sbj_nm)
+            if sbj.exists():
+                sbj.delete(delete_files=True)
+                logger.debug("Deleted the QIN pipeline test subject from XNAT: %s" % sbj_nm)
+            sbj_dir_dict[sbj] = os.path.join(source, d)
+            logger.debug("Discovered QIN pipeline test subject subdirectory: %s" % d)
+    return sbj_dir_dict
+
+def clear_xnat_subjects(subjects):
+    """
+    Deletes the given XNAT subjects, if they exist.
+    
+    @param subjects: the XNAT subjects
+    """
+    
+    for sbj in subjects:
+        if sbj.exists():
+            label = sbj.label()
+            sbj.delete(delete_files=True)
+            logger.debug("Deleted the QIN pipeline test subject from XNAT: %s" % label)
--- test/unit/pipelines/test_qipipeline.py
+++ test/unit/pipelines/test_qipipeline.py
@@ -1,6 +1,5 @@
-import os, sys, re, glob, shutil
+import os, sys, shutil
 from nose.tools import *
-import pyxnat
 import tempfile
 
 import logging
@@ -26,56 +25,51 @@ from qipipe.pipelines import QIPipeline
 from qipipe.helpers.dicom_helper import iter_dicom
 from qipipe.helpers.xnat_helper import XNAT
 from qipipe.staging import airc_collection as airc
-from qipipe.staging.staging_helper import SUBJECT_FMT
+from test.unit.pipelines.pipelines_helper import get_xnat_subjects, clear_xnat_subjects
 
 class TestPipeline:
     """Pipeline unit tests."""
     
-    def test_pipeline(self):
-        """
-        Run the pipeline and verify that scans are created in XNAT.
-        This test does not verify the CTP staging area nor that the
-        image files are correctly uploaded. These features are
-        verified by visual inspection.
-        """
+    def setUp(self):
         shutil.rmtree(RESULTS, True)
         self.xnat = XNAT().interface
-        self._test_collection('Sarcoma')
+    
+    def tearDown(self):
+        shutil.rmtree(RESULTS, True)
+    
+    def test_breast(self):
         self._test_collection('Breast')
     
+    def test_sarcoma(self):
+        self._test_collection('Sarcoma')
+    
     def _test_collection(self, collection):
+        """
+        Run the pipeline on the given collection and verify the following:
+            - scans are created in XNAT
+        
+        @param collection: the AIRC collection name
+        @attention: This test does not verify the CTP staging area nor that the
+        image files are correctly uploaded. These features should be
+        verified manually.
+        """
+
         airc_coll = airc.collection_with_name(collection)
         src = os.path.join(FIXTURE, collection.lower())
         logger.debug("Testing the QIN pipeline on %s..." % src)
 
-        # The test subjects.
-        subjects = []
-        # The input directories.
-        sbj_dirs = []
-        for d in os.listdir(src):
-            match = re.match(airc_coll.subject_pattern, d)
-            if match:
-                # The XNAT subject label.
-                sbj_nm = SUBJECT_FMT % (collection, int(match.group(1)))
-                logger.debug("Checking whether the test subject %s exists in XNAT..." % sbj_nm)
-                # Delete the XNAT subject, if necessary.
-                sbj = self.xnat.select('/project/QIN/subject/' + sbj_nm)
-                subjects.append(sbj)
-                if sbj.exists():
-                    sbj.delete(delete_files=True)
-                    logger.debug("Deleted the QIN pipeline test subject from XNAT: %s" % sbj_nm)
-                sbj_dirs.append(os.path.join(src, d))
-                logger.debug("Discovered QIN pipeline test subject subdirectory: %s" % d)
+        # The test subject => directory dictionary.
+        sbj_dir_dict = get_xnat_subjects(collection, src)
+        # Delete any existing test subjects.
+        clear_xnat_subjects(sbj_dir_dict.iterkeys())
 
         # Run the pipeline.
         dest = os.path.join(RESULTS, collection.lower())
         work_dir = tempfile.mkdtemp()
         logger.debug("Executing the QIN pipeline in %s..." % work_dir)
-        # Run the staging and stack pipeline.
-        
         sessions = qip.run(collection, dest=dest, base_dir=work_dir,
-            components = [QIPipeline.STAGING, QIPipeline.STACK], *sbj_dirs)
-
+            components = [QIPipeline.STAGING, QIPipeline.STACK], *sbj_dir_dict.itervalues())
+        
         # Verify the result.
         for sess_nm in sessions:
             sess = self.xnat.select('/project/QIN/experiment/' + sess_nm)
@@ -84,10 +78,9 @@ class TestPipeline:
             assert_is_not_none(sbj_id, "Session does not have a subject: %s" % sess_nm)
             scans = self.xnat.select('/project/QIN/subject/' + sbj_id + '/experiment/' + sess_nm + '/scans').get()
             assert_not_equal(0, len(scans), "Session does not scans: %s" % sess_nm)
+        
         # Cleanup.
-        for sbj in subjects:
-            sbj.delete(delete_files=True)
-        shutil.rmtree(RESULTS, True)
+        clear_xnat_subjects(sbj_dir_dict.iterkeys())
         shutil.rmtree(work_dir, True)
 
 
