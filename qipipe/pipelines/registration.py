ba9cb142afe3e5fbc186c601ff450bc1d682c578
FredLoney <loneyf@ohsu.edu>
2013-04-10 16:39:49 -0700

    Cosmetic changes.

--- qipipe/pipelines/registration.py
+++ qipipe/pipelines/registration.py
@@ -6,7 +6,6 @@ def create_registration_connections(input_node, input_field):
     """
     Creates the series stack connections.
     
-    @param series_spec: the series specification node
     @param input_node: the input image files source node
     @param input_field: the input image files field name
     @return: the registration workflow connections
@@ -22,10 +21,18 @@ def create_registration_connections(input_node, input_field):
     
     # Store the registration result in XNAT.
     store_reg = pe.Node(XNATUpload(project='QIN'), name='store_registration')
+
+
+    # Test node.
+    # TODO - enable template, disconnect moving and 'first' def
+    moving = pe.Node(Glue(input_names=['in_files'], output_names=['rest'], function=rest), name='rest')
+
     
     return [
         (input_node, reg_template, [(input_field, 'in_files')]),
-        (input_node, registration, [(input_field, 'moving_image')]),
+        # (input_node, registration, [(input_field, 'moving_image')]),
+        (input_node, rest, [(input_field, 'in_files')]),
+        (rest, registration, [('rest', 'moving_image')]),
         (reg_template, registration, [('template', 'fixed_image')]),
         (registration, store_reg, [('out_file', 'in_files')])]
 
@@ -38,6 +45,9 @@ def create_template_interface():
     @return: the ANTS template generation interface with default parameter settings
     """
     return Glue(input_names=['in_files'], output_names=['template'], function=first)
+
+def rest(iterable):
+    return list(iterable)[1:]
 
 def create_registration_interface():
     """
