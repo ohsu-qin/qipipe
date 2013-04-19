df4eeeb7b188931c5d84838d1b44e84a4f41de24
FredLoney <loneyf@ohsu.edu>
2013-04-19 13:45:31 -0700

    Add method doc.

--- qipipe/staging/sarcoma_config.py
+++ qipipe/staging/sarcoma_config.py
@@ -4,11 +4,22 @@ from ConfigParser import ConfigParser as Config
 
 _CFG_FILE = os.path.join(os.path.dirname(__file__), '..', '..', 'conf', 'sarcoma.cfg')
 
-def sarcoma_location(sbj_id):
-    return sarcoma_config().get('Tumor Location', sbj_id)
+def sarcoma_location(subject):
+    """
+    @param subject: the XNAT Subject ID
+    @return: the subject tumor location
+    """
+
+    return sarcoma_config().get('Tumor Location', subject)
 
 def sarcoma_config():
+    """
+    @return: the sarcoma configuration
+    @rtype: ConfigParser
+    """
+    
     if not hasattr(sarcoma_config, 'instance'):
         sarcoma_config.instance = Config()
         sarcoma_config.instance.read(_CFG_FILE)
+    
     return sarcoma_config.instance
