a292c3486e88800a4684e347776fbc83635b4468
FredLoney <loneyf@ohsu.edu>
2013-04-19 13:46:28 -0700

    Pull out setup/teardown.

--- test/unit/helpers/test_edit_dicom.py
+++ test/unit/helpers/test_edit_dicom.py
@@ -7,31 +7,39 @@ from qipipe.helpers.dicom_helper import edit_dicom_headers
 from qipipe.helpers.dicom_helper import iter_dicom
 from dicom import datadict as dd
 
-# The test parent directory.
 ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '..'))
-# The test fixture.
+"""The test parent directory."""
+
 FIXTURE = os.path.join(ROOT, 'fixtures', 'helpers', 'edit_dicom')
-# The test results.
-RESULT = os.path.join(ROOT, 'results', 'helpers', 'edit_dicom')
+"""The test fixture."""
+
+RESULTS = os.path.join(ROOT, 'results', 'helpers', 'edit_dicom')
+"""The test results directory."""
 
 class TestEditDicom:
     """DICOM edit unit tests."""
     
+    def setUp(self):
+        shutil.rmtree(RESULTS, True)
+        
+    def tearDown(self):
+        shutil.rmtree(RESULTS, True)
+    
     def test_edit_dicom_files(self):
-        # Clear the result before starting.
-        shutil.rmtree(RESULT, True)
         # The tag name => value map.
         tnv = dict(PatientID='Test Patient', BodyPartExamined='BREAST', PixelData='')
+        
         # The tag => value map.
         tv = {dd.tag_for_name(name): value for name, value in tnv.iteritems()}
-        shutil.rmtree(RESULT, True)
-        files = set(edit_dicom_headers(FIXTURE, RESULT, **tnv))
-        for ds in iter_dicom(RESULT):
+        
+        # Edit the headers.
+        files = set(edit_dicom_headers(RESULTS, FIXTURE, **tnv))
+        
+        # Verify the result.
+        for ds in iter_dicom(RESULTS):
             assert_true(ds.filename in files)
             for t, v in tv.iteritems():
                 assert_equal(v, ds[t].value)
-        # Cleanup.
-        shutil.rmtree(RESULT, True)
 
 
 if __name__ == "__main__":
