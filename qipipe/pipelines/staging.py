8257748cdb295ededab53b92a6774fe1035f8105
FredLoney <loneyf@ohsu.edu>
2013-05-28 17:29:46 -0700

    Replace Glue interface by more restrictive Unpack.

--- qipipe/pipelines/staging.py
+++ qipipe/pipelines/staging.py
@@ -2,7 +2,7 @@ import os
 import nipype.pipeline.engine as pe
 from nipype.interfaces.utility import IdentityInterface, Function
 from nipype.interfaces.dcmstack import DcmStack
-from ..interfaces import Glue, FixDicom, Compress, MapCTP, XNATUpload
+from ..interfaces import Unpack, FixDicom, Compress, MapCTP, XNATUpload
 from ..staging.staging_error import StagingError
 from ..staging.staging_helper import iter_new_visits, group_dicom_files_by_series
 from ..helpers import xnat_helper
@@ -30,31 +30,31 @@ def run(collection, *subject_dirs, **opts):
     logger.debug("%d new visits were detected" % len(new_visits))
     
     # Group the DICOM files by series.
-    scan_specs = _group_sessions_by_series(*new_visits)
+    series_specs = _group_sessions_by_series(*new_visits)
 
     # Make the staging workflow.
-    wf = _create_workflow(collection, *scan_specs, **opts)
+    workflow = _create_workflow(collection, *series_specs, **opts)
     
     # If debug is set, then diagram the staging workflow graph.
     if logger.level <= logging.DEBUG:
-        if wf.base_dir:
-            grf = os.path.join(wf.base_dir, 'staging.dot')
+        if workflow.base_dir:
+            grf = os.path.join(workflow.base_dir, 'staging.dot')
         else:
             grf = 'staging.dot'
-        wf.write_graph(dotfilename=grf)
+        workflow.write_graph(dotfilename=grf)
         logger.debug("The staging workflow graph is depicted at %s.png." % grf)
     
     # Run the staging workflow.
     with xnat_helper.connection():
-        wf.run()
+        workflow.run()
     
     # Return the new XNAT (subject, session) tuples.
     return [(sbj, sess) for sbj, sess, _ in new_visits]
 
-def _create_workflow(collection, *scan_specs, **opts):
+def _create_workflow(collection, *series_specs, **opts):
     """
     @param collection: the AIRC image collection name 
-    @param scan_specs: the (subject, session, scan, dicom_files) tuples to stage
+    @param series_specs: the (subject, session, scan, dicom_files) tuples to stage
     @param opts: the workflow options
     @keyword dest: the destination directory (default current working directory)
     @return: the staging workflow, or None if there are no new images
@@ -64,66 +64,73 @@ def _create_workflow(collection, *scan_specs, **opts):
         msg = msg + ' with options %s...' % opts
     logger.debug("%s...", msg)
 
+    # The staging location.
+    dest = os.path.abspath(opts.pop('dest', None) or os.getcwd())
+
     # The workflow.
-    wf = pe.Workflow(name='staging', **opts)
+    workflow = pe.Workflow(name='staging', **opts)
     
     # The subjects with new sessions.
-    subjects = {sbj for sbj, _, _, _ in scan_specs}
+    subjects = {sbj for sbj, _, _, _ in series_specs}
 
-    # The iterable series specification.
-    glue = Glue(input_names=['scan_spec'], output_names=['subject', 'session', 'scan', 'dicom_files'])
-    scan_spec = pe.Node(glue, name='scan_spec')
-    scan_spec.iterables = ('scan_spec', scan_specs)
+    # The workflow subject inputs.
+    subject_spec = pe.Node(IdentityInterface(fields=['collection', 'dest', 'subjects']),
+        name='subject_spec')
+    subject_spec.inputs.collection = collection
+    subject_spec.inputs.dest = dest
+    subject_spec.inputs.subjects = subjects
 
-    # The staging location.
-    dest = os.path.abspath(opts.pop('dest', os.getcwd()))
-
-    # The workflow input.
-    input_spec = pe.Node(IdentityInterface(fields=['collection', 'dest', 'subjects']),
-        name='input_spec')
-    input_spec.inputs.collection = collection
-    input_spec.inputs.dest = dest
-    input_spec.inputs.subjects = subjects
+    # The iterable series inputs.
+    series_spec_xf = Unpack(input_name='series_spec', output_names=['subject', 'session', 'scan', 'dicom_files'])
+    series_spec = pe.Node(series_spec_xf, name='series_spec')
+    series_spec.iterables = ('series_spec', series_specs)
 
     # Map each QIN Patient ID to a CTP Patient ID.
     map_ctp = pe.Node(MapCTP(), name='map_ctp')
+    workflow.connect(subject_spec, 'collection', map_ctp, 'collection')
+    workflow.connect(subject_spec, 'subjects', map_ctp, 'patient_ids')
+    workflow.connect(subject_spec, 'dest', map_ctp, 'dest')
 
     # The CTP staging directory factory.
     ctp_dir_func = Function(input_names=['dest', 'subject', 'session', 'series'], dest=dest,
         output_names=['out_dir'], function=_ctp_series_directory)
     ctp_dir = pe.Node(ctp_dir_func, name='ctp_dir')
-    ctp_dir.inputs.dest = dest
+    workflow.connect(subject_spec, 'dest', ctp_dir, 'dest')
+    workflow.connect(series_spec, 'subject', ctp_dir, 'subject')
+    workflow.connect(series_spec, 'session', ctp_dir, 'session')
+    workflow.connect(series_spec, 'scan', ctp_dir, 'series')
 
     # Fix the AIRC DICOM tags.
     fix_dicom = pe.Node(FixDicom(collection=collection), name='fix_dicom')
+    workflow.connect(series_spec, 'subject', fix_dicom, 'subject')
+    workflow.connect(series_spec, 'dicom_files', fix_dicom, 'in_files')
 
-    # Compress the fixed DICOM files.
-    compress = pe.MapNode(Compress(), iterfield='in_file', name='compress')
+    # Compress each fixed DICOM file for a given series.
+    # The result is a list of the compressed files for the series.
+    compress_dicom = pe.MapNode(Compress(), iterfield='in_file', name='compress_dicom')
+    workflow.connect(fix_dicom, 'out_files', compress_dicom, 'in_file')
+    workflow.connect(ctp_dir, 'out_dir', compress_dicom, 'dest')
 
-    # Store the fixed DICOM files in XNAT.
-    store_dicom = pe.Node(XNATUpload(project='QIN', format='DICOM'), name='store_dicom')
+    # Store the compressed scan DICOM files in XNAT.
+    upload_dicom = pe.Node(XNATUpload(project='QIN', format='DICOM'), name='upload_dicom')
+    workflow.connect(series_spec, 'subject', upload_dicom, 'subject')
+    workflow.connect(series_spec, 'session', upload_dicom, 'session')
+    workflow.connect(series_spec, 'scan', upload_dicom, 'scan')
+    workflow.connect(compress_dicom, 'out_file', upload_dicom, 'in_files')
 
     # Stack the scan.
     stack = pe.Node(DcmStack(embed_meta=True, out_format="series%(SeriesNumber)03d"),
         name='stack')
+    workflow.connect(fix_dicom, 'out_files', stack, 'dicom_files')
     
     # Store the stack files in XNAT.
-    store_stack = pe.Node(XNATUpload(project='QIN', format='NIFTI'), name='store_stack')
+    upload_stack = pe.Node(XNATUpload(project='QIN', format='NIFTI'), name='upload_stack')
+    workflow.connect(series_spec, 'subject', upload_stack, 'subject')
+    workflow.connect(series_spec, 'session', upload_stack, 'session')
+    workflow.connect(series_spec, 'scan', upload_stack, 'scan')
+    workflow.connect(stack, 'out_file', upload_stack, 'in_files')
     
-    wf.connect([
-        (input_spec, map_ctp, [('collection', 'collection'), ('subjects', 'patient_ids'), ('dest', 'dest')]),
-        (input_spec, ctp_dir, [('dest', 'dest')]),
-        (scan_spec, ctp_dir, [('subject', 'subject'), ('session', 'session'), ('scan', 'series')]),
-        (scan_spec, fix_dicom, [('subject', 'subject'), ('dicom_files', 'in_files')]),
-        (scan_spec, store_dicom, [('subject', 'subject'), ('session', 'session'), ('scan', 'scan')]),
-        (scan_spec, store_stack, [('subject', 'subject'), ('session', 'session'), ('scan', 'scan')]),
-        (ctp_dir, compress, [('out_dir', 'dest')]),
-        (fix_dicom, compress, [('out_files', 'in_file')]),
-        (fix_dicom, stack, [('out_files', 'dicom_files')]),
-        (compress, store_dicom, [('out_file', 'in_files')]),
-        (stack, store_stack, [('out_file', 'in_files')])])
-    
-    return wf
+    return workflow
 
 def _group_sessions_by_series(*session_specs):
     """
---
+++ test/unit/interfaces/test_unpack.py
@@ -0,0 +1,36 @@
+import os, sys, re
+from nose.tools import *
+from nipype.interfaces.base import Undefined
+
+import logging
+logger = logging.getLogger(__name__)
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+from qipipe.interfaces.unpack import Unpack
+
+ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+"""The test parent directory."""
+
+RESULTS = os.path.join(ROOT, 'results', 'interfaces', 'unpack')
+"""The test results directory."""
+
+from nipype import config
+cfg = dict(logging=dict(workflow_level='DEBUG', log_directory=RESULTS, log_to_file=True),
+    execution=dict(crashdump_dir=RESULTS, create_report=False))
+config.update_config(cfg)
+
+class TestUnpack:
+    """Unpack interface unit tests."""
+    
+    def test_unpack(self):
+        unpack = Unpack(input_name='list', output_names=['a', 'b'])
+        unpack.inputs.list = [1, 2]
+        result = unpack.run()
+        assert_equals(1, result.outputs.a, "Output field a incorrect: %s" % result.outputs.a)
+        assert_equals(2, result.outputs.b, "Output field b incorrect: %s" % result.outputs.b)
+
+
+if __name__ == "__main__":
+    import nose
+    
+    nose.main(defaultTest=__name__)
