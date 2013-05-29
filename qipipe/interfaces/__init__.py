e58688d87ba1c4af718ea3e427d94f68c3df3b16
FredLoney <loneyf@ohsu.edu>
2013-05-28 17:31:50 -0700

    Replace Glue interface by more restrictive Unpack.

--- qipipe/interfaces/__init__.py
+++ qipipe/interfaces/__init__.py
@@ -4,7 +4,9 @@ from .fix_dicom import FixDicom
 from .group_dicom import GroupDicom
 from .map_ctp import MapCTP
 from .move import Move
-from .glue import Glue
+from .unpack import Unpack
 from .uncompress import Uncompress
 from .xnat_upload import XNATUpload
 from .xnat_download import XNATDownload
+from .fastfit import Fastfit
+from .mri_volcluster import MriVolCluster
