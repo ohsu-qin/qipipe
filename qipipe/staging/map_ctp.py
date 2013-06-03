5aee445745a859dfe2b85f07b4cf88f40e41fb6f
FredLoney <loneyf@ohsu.edu>
2013-06-03 16:46:33 -0700

    Clarify the patient id regexp.

--- qipipe/staging/map_ctp.py
+++ qipipe/staging/map_ctp.py
@@ -30,8 +30,12 @@ class CTPPatientIdMap(dict):
     to build the map and a L{write} output method to print the CTP map properties.
     """
     
-    AIRC_PAT = '(\w+?)(\d+)'
-    """The input Patient ID pattern."""
+    AIRC_PAT = re.compile("""
+        ([a-zA-Z]+)     # The study name
+        _?              # An optional underscore delimiter
+        (\d+)$          # The patient number
+    """, re.VERBOSE)
+    """The input Patient ID pattern is the study name followed by a number, e.g. C{Breast010}."""
     
     CTP_FMT = '%s-%04d'
     """The CTP Patient ID format with arguments (CTP collection name, input Patient ID number)."""
@@ -48,13 +52,18 @@ class CTPPatientIdMap(dict):
 
         @param collection: the AIRC collection name 
         @param patient_ids: the DICOM Patient IDs to map
+        @raise ValueError: if an input patient id format is not the study followed by the
+            patient number
         """
         ctp_coll = ctp_collection_for(collection)
-        for qin_id in patient_ids:
-            pt_nbr = int(re.match(CTPPatientIdMap.AIRC_PAT, qin_id).group(2))
+        for in_pt_id in patient_ids:
+            match = CTPPatientIdMap.AIRC_PAT.match(in_pt_id)
+            if not match:
+                raise ValueError("Unsupported input QIN patient id format: %s" % in_pt_id)
+            pt_nbr = int(match.group(2))
             ctp_id = CTPPatientIdMap.CTP_FMT % (ctp_coll, pt_nbr)
-            self[qin_id] = ctp_id
-            logger.debug(CTPPatientIdMap.MSG_FMT % (qin_id, ctp_id))
+            self[in_pt_id] = ctp_id
+            logger.debug(CTPPatientIdMap.MSG_FMT % (in_pt_id, ctp_id))
     
     def write(self, dest=sys.stdout):
         """
