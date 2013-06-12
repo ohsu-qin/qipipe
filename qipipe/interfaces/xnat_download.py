7fbf7301263ca2a95623e90856c0c7bd2b9520c7
FredLoney <loneyf@ohsu.edu>
2013-06-12 10:25:19 -0700

    Make the format optional.

--- qipipe/interfaces/xnat_download.py
+++ qipipe/interfaces/xnat_download.py
@@ -11,17 +11,19 @@ class XNATDownloadInputSpec(BaseInterfaceInputSpec):
 
     session = traits.Str(mandatory=True, desc='The XNAT session name')
     
-    container_type = traits.Str(mandatory=True, desc='The XNAT resource container type, e.g. scan or reconstruction')
+    scan = traits.Either(traits.Str, traits.Int, desc='The XNAT scan resource container name')
     
-    format = traits.Str(mandatory=True, desc='The XNAT image format (NIFIT or DICOM)')
+    reconstruction = traits.Str(desc='The XNAT reconstruction resource container name')
+    
+    analysis = traits.Str(desc='The XNAT assessment resource container name')
+    
+    format = traits.Enum('NIFTI', 'DICOM', desc='The XNAT image format (default NIFTI)')
     
     dest = Directory(desc='The download location')
 
 
 class XNATDownloadOutputSpec(TraitedSpec):
     out_files = OutputMultiPath(File(exists=True), desc='The downloaded files')
-    
-    format = traits.Str(desc='The XNAT image format, optional unless the input file extension is missing')
 
 
 class XNATDownload(BaseInterface):
@@ -30,13 +32,24 @@ class XNATDownload(BaseInterface):
     output_spec = XNATDownloadOutputSpec
 
     def _run_interface(self, runtime):
+        opts = {}
+        for ctr_type in ['scan', 'reconstruction', 'analysis']:
+            ctr_name = getattr(self.inputs, ctr_type)
+            if ctr_name:
+                opts[ctr_type] = ctr_name
+                break
+        if self.inputs.dest:
+            opts['dest'] = self.inputs.dest
+        if self.inputs.format:
+            opts['format'] = self.inputs.format
         with xnat_helper.connection() as xnat:
-            self._out_files = xnat.download(self.inputs.project, self.inputs.subject,
-                self.inputs.session, format=self.inputs.format, container_type=self.inputs.container_type,
-                dest=self.inputs.dest)
+            self._out_files = xnat.download(self.inputs.project,
+                self.inputs.subject, self.inputs.session, **opts)
+        
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
         outputs['out_files'] = self._out_files
+        
         return outputs
