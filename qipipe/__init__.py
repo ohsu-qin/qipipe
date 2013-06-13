e330ba7471d2887f5bf3eaa656850afc13350bc9
FredLoney <loneyf@ohsu.edu>
2013-06-13 12:10:56 -0700

    Set version in init.

--- qipipe/__init__.py
+++ qipipe/__init__.py
@@ -1,3 +1,7 @@
-PROJECT = 'QIN'
-
 from . import helpers, interfaces, pipelines, staging
+
+__version__ = '2.1.2'
+"""The one-based major.minor.patch version."""
+
+PROJECT = 'QIN'
+"""The XNAT project id."""
