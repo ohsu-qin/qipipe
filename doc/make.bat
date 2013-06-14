787741325a94793c8b2c164d8694c176e08c54c5
FredLoney <loneyf@ohsu.edu>
2013-06-14 10:49:59 -0700

    Simplify the make.

--- doc/make.bat
+++ doc/make.bat
@@ -6,24 +6,12 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set BUILDDIR=_build
-set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
-set I18NSPHINXOPTS=%SPHINXOPTS% .
-if NOT "%PAPER%" == "" (
-	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
-	set I18NSPHINXOPTS=-D latex_paper_size=%PAPER% %I18NSPHINXOPTS%
-)
+set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% source
 
 if "%1" == "" (
   set TARGET=html
 ) else (
   set TARGET=%1)
-
-if "%TARGET" == "help" (
-	:help
-	echo.Please use `make ^<target^>` where ^<target^> is one of
-	echo.  html       to make standalone HTML files
-	goto end
-)
 
 if "%TARGET" == "clean" (
 	for /d %%i in (%BUILDDIR%\*) do rmdir /q /s %%i
