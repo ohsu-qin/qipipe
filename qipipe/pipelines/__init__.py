dab99d72e65d57f2ceecccdcf577fe78489a4b8b
FredLoney <loneyf@ohsu.edu>
2013-07-05 17:08:16 -0700

    Cosmetic changes.

--- qipipe/pipelines/__init__.py
+++ qipipe/pipelines/__init__.py
@@ -6,7 +6,7 @@ The ``pipelines`` module includes the following QIN pipelines:
 - :mod:`.staging`: executes the staging workflow to detect new images, group them by
   series, import them into XNAT and prepare them for TCIA import
   
-- :mod:`.registration`: mask the target tissue and corrects motion artifacts
+- :mod:`.registration`: masks the target tissue and corrects motion artifacts
 
-- :mod:`.modeling`: perform pharmokinetic modeling
+- :mod:`.modeling`: performs pharmokinetic modeling
 """
