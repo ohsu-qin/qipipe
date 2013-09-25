c0fba08140a88c8345a016237409cd3b500510eb
FredLoney <loneyf@ohsu.edu>
2013-09-25 14:50:17 -0700

    Use relative import.

--- qipipe/__init__.py
+++ qipipe/__init__.py
@@ -1,6 +1,6 @@
 """The ``qipipe`` module includes the QIN Python source modules."""
 
-from helpers.project import project
+from .helpers.project import project
 from . import (helpers, interfaces, pipeline, staging)
 
 __version__ = '3.2.2'
