854283e53c5ff380f82fd9c9467a074953076e0f
FredLoney <loneyf@ohsu.edu>
2013-11-22 15:49:05 -0800

    Support directory copy.

--- qipipe/interfaces/copy.py
+++ qipipe/interfaces/copy.py
@@ -1,7 +1,7 @@
 import os
 import shutil
-from nipype.interfaces.base import (traits,
-                                    BaseInterfaceInputSpec, TraitedSpec, BaseInterface,
+from nipype.interfaces.base import (traits, BaseInterfaceInputSpec,
+                                    TraitedSpec, BaseInterface,
                                     File, Directory)
 
 
@@ -9,12 +9,15 @@ class CopyInputSpec(BaseInterfaceInputSpec):
     in_file = traits.Either(File, Directory, exists=True, mandatory=True,
                             desc='The file or directory to copy')
 
-    dest = traits.Either(File, Directory, mandatory=True,
-                         desc='The destination path')
+    dest = Directory(mandatory=True, desc='The destination directory path')
 
+    out_fname = traits.Either(File, Directory,
+                              desc='The destination file name'
+                                   ' (default is the input file name)')
 
 class CopyOutputSpec(TraitedSpec):
-    out_file = File(exists=True, desc='The copied file or directory')
+    out_file = traits.Either(File, Directory, exists=True,
+                             desc='The copied file or directory')
 
 
 class Copy(BaseInterface):
@@ -25,26 +28,35 @@ class Copy(BaseInterface):
     output_spec = CopyOutputSpec
 
     def _run_interface(self, runtime):
-        self.out_file = self._copy(self.inputs.in_file, self.inputs.dest)
+        self._out_file = self._copy(self.inputs.in_file, self.inputs.dest,
+                                   self.inputs.out_fname)
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['out_file'] = self.out_file
+        outputs['out_file'] = self._out_file
+
         return outputs
 
-    def _copy(self, in_file, dest):
+    def _copy(self, in_file, dest, out_fname=None):
         """
-        Copys the given file.
+        Copies the given file.
     
-        :param in_file: the path of the file to copy
-        @parma dest: the destination directory path
-        :return: the copyd file path
+        :param in_file: the path of the file or directory to copy
+        :param dest: the destination directory path
+        :param out_fname: the destination file name
+            (default is the input file name)
+        :return: the copied file path
         """
         dest = os.path.abspath(dest)
         if not os.path.exists(dest):
             os.makedirs(dest)
-        shutil.copy(in_file, dest)
-        _, fname = os.path.split(in_file)
-        out_file = os.path.join(dest, fname)
+        if not out_fname:
+            _, out_fname = os.path.split(in_file)
+        out_file = os.path.join(dest, out_fname)
+        if os.path.isdir(in_file):
+            shutil.copytree(in_file, out_file)
+        else:
+            shutil.copy(in_file, out_file)
+        
         return out_file
