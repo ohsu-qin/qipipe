aff745a0eec691fcb68d85f4369aaae175eba478
FredLoney <loneyf@ohsu.edu>
2013-12-13 12:17:05 -0800

    Add destination.

--- test/unit/pipeline/test_registration.py
+++ test/unit/pipeline/test_registration.py
@@ -36,14 +36,11 @@ class TestRegistrationWorkflow(StagedTestBase):
         super(TestRegistrationWorkflow, self).__init__(
             logger(__name__), FIXTURES, RESULTS, use_mask=True)
 
-    # def test_breast_with_ants(self):
-    #     self._test_breast()
-    # 
-    # def test_sarcoma_with_ants(self):
-    #     self._test_sarcoma()
-
-    def test_breast_with_fnirt(self):
-        self._test_breast(technique='fnirt')
+    def test_breast(self):
+        super(TestRegistrationWorkflow, self)._test_breast(technique='mock')
+    
+    def test_sarcoma(self):
+        super(TestRegistrationWorkflow, self)._test_sarcoma(technique='mock')
 
     def _run_workflow(self, fixture, subject, session, images, mask, **opts):
         """
@@ -58,17 +55,37 @@ class TestRegistrationWorkflow(StagedTestBase):
         :return: the :meth:`qipipe.pipeline.registration.run` result
         """
         self._logger.debug("Testing the registration workflow on %s..." %
-                               fixture)
+                           fixture)
         # A reasonable bolus uptake index.
         bolus_arv_ndx = min(3, len(images) / 3)
+        # The target location.
+        dest = os.path.join(RESULTS, subject, session)
+        # Execute the workflow.
         return registration.run(subject, session, images,
                                 bolus_arrival_index=bolus_arv_ndx, mask=mask,
-                                cfg_file=REG_CONF, resource=RESOURCE, **opts)
+                                cfg_file=REG_CONF, resource=RESOURCE, dest=dest,
+                                **opts)
 
     def _verify_result(self, xnat, subject, session, result):
-        rsc_obj = xnat.find(project(), sbj, sess, resource=RESOURCE)
-        assert_is_not_none(rsc_obj, "The %s %s %s XNAT registration resource"
-                           " object was not created" % (sbj, sess, result))
+        # Confirm that the XNAT resource object was created.
+        rsc_obj = xnat.find(project(), subject, session, resource=RESOURCE)
+        assert_is_not_none(rsc_obj,
+                           "The %s %s %s XNAT registration resource object was"
+                           " not created" % (subject, session, RESOURCE))
+        out_split = [os.path.split() for f in result]
+        out_dirs, out_files = zip(out_split)
+        rsc_files = rsc_obj.files().get()
+        assert_equals(out_files, rsc_files,
+                      "The %s %s %s XNAT registration resource files are incorrect"
+                      ": %s" % (subject, session, RESOURCE, rsc_files))
+        
+        # Confirm that the output files were created.
+        dest = os.path.join(RESULTS, subject, session)
+        dest_files = [os.path.join(dest, f) for f in os.listdir(dest)]
+        assert_equals(set(dest_files), set(result),
+                      "The %s %s %s XNAT registration result is incorrect:"
+                      " %s" % (subject, session, RESOURCE, result))
+
 
 
 if __name__ == "__main__":
