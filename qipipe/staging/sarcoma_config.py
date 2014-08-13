457dd1616a953506df8833846f70d89f5e722923
FredLoney <loneyf@ohsu.edu>
2014-08-13 10:35:05 -0700

    Issue more readable exception if the value is not found.

--- qipipe/staging/sarcoma_config.py
+++ qipipe/staging/sarcoma_config.py
@@ -1,8 +1,21 @@
 import os
 from ConfigParser import ConfigParser as Config
+from ConfigParser import NoOptionError
 
-_CFG_FILE = os.path.join(
-    os.path.dirname(__file__), '..', '..', 'conf', 'sarcoma.cfg')
+CFG_FILE = os.path.abspath(
+    os.path.join( os.path.dirname(__file__), '..', '..', 'conf', 'sarcoma.cfg')
+)
+"""
+The Sarcoma Tumor Location configuration file. This file contains
+properties that associat the subject name to the location, e.g.::
+
+    Sarcoma004 = SHOULDER
+
+The value is the SNOMED anatomy term.
+"""
+
+class ConfigError(Exception):
+    pass
 
 
 def sarcoma_location(subject):
@@ -11,7 +24,11 @@ def sarcoma_location(subject):
     :return: the subject tumor location
     """
 
-    return sarcoma_config().get('Tumor Location', subject)
+    try
+      return sarcoma_config().get('Tumor Location', subject)
+    except NoOptionError:
+      raise ConfigError("Tumor location for subject %s was not found in the"
+                        " sarcoma configuration file %s" % (subject, CFG_FILE))
 
 
 def sarcoma_config():
@@ -19,8 +36,9 @@ def sarcoma_config():
     :return: the sarcoma configuration
     :rtype: ConfigParser
     """
+    # Read the configuration file on demand.
     if not hasattr(sarcoma_config, 'instance'):
         sarcoma_config.instance = Config()
-        sarcoma_config.instance.read(_CFG_FILE)
+        sarcoma_config.instance.read(CFG_FILE)
 
     return sarcoma_config.instance
