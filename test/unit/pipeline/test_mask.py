a7620a158aa8c6744bc84fe443c0eb2dfe9aca10
FredLoney <loneyf@ohsu.edu>
2014-12-08 10:06:28 -0800

    Disable test pending time series fixture.

--- test/unit/pipeline/test_mask.py
+++ test/unit/pipeline/test_mask.py
@@ -20,34 +20,39 @@ RESULTS = os.path.join(ROOT, 'results', 'pipeline', 'mask')
 
 
 class TestMaskWorkflow(StagedTestBase):
-
     """
     Mask workflow unit tests.
     
     This test exercises the mask workflow on three series of one visit in each
     of the Breast and Sarcoma studies.
+    
+    :Note: This test is disabled pending a time series test fixture.
+     
+     TODO - Make a time series test fixture and enable the tests.
     """
 
     def __init__(self):
         super(TestMaskWorkflow, self).__init__(
             logger(__name__), FIXTURES, RESULTS)
 
-    def test_breast(self):
-        self._test_breast()
+    # def test_breast(self):
+    #     self._test_breast()
+    # 
+    # def test_sarcoma(self):
+    #     self._test_sarcoma()
 
-    def test_sarcoma(self):
-        self._test_sarcoma()
-
-    def _run_workflow(self, fixture, *inputs, **opts):
+    def _run_workflow(self, fixture, subject, session, **opts):
         """
         Executes :meth:`qipipe.pipeline.mask.run` on the given input.
         
         :param fixture: the test fixture directory
-        :param inputs: the input scans
+        :param subject: the input subject
+        :param session: the input session
         :param opts: the target workflow options
         :return: the :meth:`qipipe.pipeline.mask.run` result
         """
         logger(__name__).debug("Testing the mask workflow on %s..." % fixture)
+        input_dict = {subject: {session: opts}}
         return mask.run(*inputs, cfg_file=MASK_CONF, **opts)
 
     def _verify_result(self, xnat, subject, session, result):
