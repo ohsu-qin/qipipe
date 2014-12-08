ad240af4dd4d6521682e55f9688f48c48e2c3727
FredLoney <loneyf@ohsu.edu>
2014-12-08 10:04:39 -0800

    Validate the download.

--- test/unit/interfaces/test_xnat_download.py
+++ test/unit/interfaces/test_xnat_download.py
@@ -2,15 +2,17 @@ import os
 import glob
 import re
 import shutil
-from nose.tools import (assert_equal, assert_true)
+from nose.tools import (assert_equal, assert_is_not_none, assert_true)
 from qipipe.interfaces import XNATDownload
 from qiutil import xnat_helper
 from ... import (project, ROOT)
 from test.helpers.logging_helper import logger
 from test.helpers.xnat_test_helper import generate_unique_name
 
-FIXTURE = os.path.join(ROOT, 'fixtures', 'interfaces', 'xnat', 'Sarcoma001', 'Session01')
-"""The test fixture directory."""
+FIXTURE = os.path.join(ROOT, 'fixtures', 'interfaces', 'xnat', 'Sarcoma001',
+                      'Session01', 'scans', '9', 'resource', 'NIFTI',
+                      'series09_t1.nii.gz')
+"""The test fixture file."""
 
 RESULTS = os.path.join(ROOT, 'results', 'interfaces', 'xnat')
 """The test results directory."""
@@ -24,6 +26,8 @@ SESSION = 'Session01'
 SCAN = 9
 """The test scan number."""
 
+RESOURCE = 'NIFTI'
+
 
 class TestXNATDownload(object):
 
@@ -34,32 +38,16 @@ class TestXNATDownload(object):
         shutil.rmtree(RESULTS, True)
 
         with xnat_helper.connection() as xnat:
-            # The XNAT test subject.
-            sbj = xnat.interface.select('/project/QIN').subject(SUBJECT)
-            # The test file objects.
-            self._file_names = set()
-            for scan_dir in glob.glob(FIXTURE + '/scan/%d' % SCAN):
-                    _, scan = os.path.split(scan_dir)
-                    scan_obj = xnat.get_scan(project(), SUBJECT, SESSION,
-                                             SCAN)
-                    rsc_obj = scan_obj.resource('DICOM')
-                    for f in glob.glob(scan_dir + '/resource/DICOM/*.dcm.gz'):
-                        _, fname = os.path.split(f)
-                        self._file_names.add(fname)
-                        file_obj = rsc_obj.file(fname)
-                        # Upload the DICOM file.
-                        file_obj.insert(f, experiments='xnat:MRSessionData',
-                                        resource='DICOM')
-                    rsc_obj = scan_obj.resource('NIFTI')
-                    for f in glob.glob(scan_dir + '/resource/NIFTI/*.nii.gz'):
-                        _, fname = os.path.split(f)
-                        self._file_names.add(fname)
-                        file_obj = rsc_obj.file(fname)
-                        # Upload the NiFTI file.
-                        file_obj.insert(f, experiments='xnat:MRSessionData',
-                                        resource='NIFTI')
-        logger(__name__).debug("Uploaded the test %s %s files %s." %
-              (SUBJECT, SESSION, list(self._file_names)))
+            # Make the XNAT scan object.
+            scan_obj = xnat.get_scan(project(), SUBJECT, SESSION, SCAN)
+            rsc_obj = scan_obj.resource(RESOURCE)
+            _, fname = os.path.split(FIXTURE)
+            file_obj = rsc_obj.file(fname)
+            # Upload the NiFTI file.
+            file_obj.insert(FIXTURE)
+        logger(__name__).debug("Uploaded the test %s %s scan %d file %s." %
+                               (SUBJECT, SESSION, SCAN, FIXTURE))
+
 
     def tearDown(self):
         xnat_helper.delete_subjects(project(), SUBJECT)
@@ -70,48 +58,20 @@ class TestXNATDownload(object):
                                "%s %s scan %d..." % (SUBJECT, SESSION, SCAN))
         # Download the files.
         download = XNATDownload(project=project(), subject=SUBJECT,
-                                session=SESSION, scan=SCAN, resource='DICOM',
+                                session=SESSION, scan=SCAN, resource=RESOURCE,
                                 dest=RESULTS)
         result = download.run()
 
-        # Verify the result
-        dl_files = result.outputs.out_files
-        assert_equal(len(dl_files), 2,
-                     "The %s download file count is incorrect: %s" %
-                            (SESSION, dl_files))
-        for f in dl_files:
-            assert_true(os.path.exists(f),
-                        "The file was not downloaded: %s" % f)
-            fdir, fname = os.path.split(f)
-            assert_true(os.path.samefile(RESULTS, fdir),
-                        "The download location is incorrect: %s" % fdir)
-            _, srcname = os.path.split(FIXTURE)
-            assert_true(fname in self._file_names,
-                        "The download file name is incorrect: %s" % fname)
-
-    def test_download_nii_scans(self):
-        logger(__name__).debug("Testing the XNATDownload interface on all"
-                               "%s %s NiFTI scans..." % (SUBJECT, SESSION))
-        # Download the files.
-        download = XNATDownload(project=project(), subject=SUBJECT,
-                                session=SESSION, container_type='scan',
-                                resource='NIFTI', dest=RESULTS)
-        result = download.run()
-
-        # Verify the result
-        dl_files = result.outputs.out_files
-        assert_equal(len(dl_files), 2,
-                     "The %s scan NiFTI download file count is incorrect: %s" %
-                            (SESSION, dl_files))
-        for f in dl_files:
-            assert_true(os.path.exists(f),
-                        "The file was not downloaded: %s" % f)
-            fdir, fname = os.path.split(f)
-            assert_true(os.path.samefile(RESULTS, fdir),
-                        "The download location is incorrect: %s" % fdir)
-            _, srcname = os.path.split(FIXTURE)
-            assert_true(fname in self._file_names,
-                        "The download file name is incorrect: %s" % fname)
+        # Verify the result.
+        dl_file = result.outputs.out_file
+        assert_is_not_none(dl_file, "The %s %s scan %s download result out_file"
+                                    " property is not set" % (SUBJECT, SESSION, SCAN))
+        assert_true(os.path.exists(dl_file), "The %s %s scan %s file was not"
+                                             " downloaded" % (SUBJECT, SESSION, SCAN))
+        fdir, fname = os.path.split(dl_file)
+        assert_true(os.path.samefile(RESULTS, fdir),
+                    "The download location is incorrect: %s" % fdir)
+        _, srcname = os.path.split(FIXTURE)
 
 
 if __name__ == "__main__":
