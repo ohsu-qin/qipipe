f03fda7e81194b234cb5635f93c7ef2362a9a15a
FredLoney <loneyf@ohsu.edu>
2015-01-30 16:03:52 -0800

    group_dicom_files_by_series moved to qidicom.

--- qipipe/staging/staging_helper.py
+++ qipipe/staging/staging_helper.py
@@ -4,11 +4,12 @@ import os
 import re
 import glob
 from collections import defaultdict
-from qixnat import project
-import qixnat
-from qidicom import reader
 from qiutil.collections import nested_defaultdict
 from qiutil.logging import logger
+import qixnat
+from qixnat import project
+from qidicom import reader
+from qidicom.hierarchy import group_dicom_files_by_series
 from . import airc_collection as airc
 from .staging_error import StagingError
 
