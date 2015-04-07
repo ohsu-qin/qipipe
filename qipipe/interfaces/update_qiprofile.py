138135a0ff8b09644c2e7b49c455b6b70f1270f3
FredLoney <loneyf@ohsu.edu>
2015-04-07 11:42:46 -0700

    qiprofile update is on the session.

--- qipipe/interfaces/update_qiprofile.py
+++ qipipe/interfaces/update_qiprofile.py
@@ -1,26 +1,16 @@
 import re
 from nipype.interfaces.base import (traits, BaseInterfaceInputSpec,
                                     BaseInterface)
-from qiutil.qiprofile_helper import QIProfile
+from ..helpers import qiprofile
 
 
 class UpdateQIProfileInputSpec(BaseInterfaceInputSpec):
+
     project = traits.Str(mandatory=True, desc='The XNAT project id')
 
     subject = traits.Str(mandatory=True, desc='The XNAT subject name')
 
     session = traits.Str(mandatory=True, desc='The XNAT session name')
-
-    session_params = traits.Dict({}, usedefault=True,
-                                 desc='The imaging acquistion parameters'
-                                      ' name-value dictionary')
-
-    resource = traits.Str(desc='The XNAT modeling resource name')
-
-    modeling_params = traits.Dict({}, usedefault=True,
-                                  desc='The PK modeling parameters'
-                                       ' name-value dictionary')
-
 
 class UpdateQIProfile(BaseInterface):
     """
@@ -31,12 +21,6 @@ class UpdateQIProfile(BaseInterface):
     input_spec = UpdateQIProfileInputSpec
 
     def _run_interface(self, runtime):
-        db = QIProfile()
-        sess = db.save_session(self.inputs.project, self.inputs.subject,
-                               self.inputs.session, **self.inputs.session_params)
-        if self.inputs.resource:
-            mdl = Modeling(session=sess, name=self.inputs.resource,
-                           **self.inputs.modeling_params)
-            mdl.save()
-
+        qiprofile.sync_session(self.inputs.project, self.inputs.subject,
+                               self.inputs.session)
         return runtime
