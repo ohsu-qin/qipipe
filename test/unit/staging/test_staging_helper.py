411e5ef416879d48403b1a69feae10137db65acf
FredLoney <loneyf@ohsu.edu>
2015-04-07 11:51:48 -0700

    Change dictionary variable name.

--- test/unit/staging/test_staging_helper.py
+++ test/unit/staging/test_staging_helper.py
@@ -10,13 +10,12 @@ FIXTURE = os.path.join(ROOT, 'fixtures', 'staging', 'sarcoma')
 
 
 class TestStagingHelper(object):
-
     """staging_helper unit tests."""
 
     def test_staging_iterator(self):
         dirs = glob.glob(FIXTURE + '/Subj*')
-        sbj_dict = {sbj: {sess: vol_dict}
-                    for sbj, sess, vol_dict in iter_stage('Sarcoma', *dirs)}
+        sbj_dict = {sbj: {sess: vol_dcm_dict}
+                    for sbj, sess, vol_dcm_dict in iter_stage('Sarcoma', *dirs)}
         expected_sbjs = set(["Sarcoma00%d" % n for n in range(1, 3)])
         actual_sbjs = set(sbj_dict.keys())
         assert_equal(actual_sbjs, expected_sbjs, "Subjects are incorrect: %s" %
@@ -26,3 +25,4 @@ class TestStagingHelper(object):
 if __name__ == "__main__":
     import nose
     nose.main(defaultTest=__name__)
+
