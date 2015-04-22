3fe4cc59bbdc3c08ebaafe048b2946cee51296b0
FredLoney <loneyf@ohsu.edu>
2015-04-21 17:23:47 -0700

    Test T2 scan iteration.

--- test/unit/staging/test_iterator.py
+++ test/unit/staging/test_iterator.py
@@ -14,17 +14,16 @@ FIXTURES = os.path.join(ROOT, 'fixtures', 'staging')
 class TestStagingIterator(object):
     """iterator unit tests."""
 
-    def setUp(self):
-        # Delete the existing test subjects, since staging only detects
-        # new visits.
-        # The test subject => directory dictionary.
-        sbj_dir_dict = subject_sources(collection, fixture)
-        subjects = sbj_dir_dict.keys()
-        with qixnat.connect() as xnat:
-            xnat.delete_subjects(project(), *SUBJECTS)
-
     def test_breast(self):
-        self._test_collection('Breast')
+        discovered = self._test_collection('Breast')
+        # The first visit has both a T1 and a T2 scan.
+        for visit in [1, 2]:
+            session = "Session0%d" % visit
+            scans = {item[2] for item in discovered if item[1] == session}
+            expected_scans = set([1, 2]) if visit == 1 else set([1])
+            assert_equal(scans, expected_scans,
+                         "Discovered Breast003 %s scans are incorrect: %s" %
+                         (session, scans))
 
     def test_sarcoma(self):
         self._test_collection('Sarcoma')
@@ -32,36 +31,34 @@ class TestStagingIterator(object):
     def _test_collection(self, collection):
         """
         Iterate on the given collection fixture subdirectory.
-
-        :Note: This test does not verify the CTP staging area nor the
-        uploaded image file content. These features should be verified
-        manually.
         
         :param collection: the AIRC collection name
         """
         fixture = os.path.join(FIXTURES, collection.lower())
-        logger(__name__).debug("Testing the staging workflow on %s..." %
+        logger(__name__).debug("Testing the staging iterator on %s..." %
                                fixture)
 
-        # The staging destination and work area.
-        dest = os.path.join(RESULTS, 'staged')
-        work = os.path.join(RESULTS, 'work')
-
-        # The test subject => directory dictionary.
+        # The test {subject: directory} dictionary.
         sbj_dir_dict = subject_sources(collection, fixture)
         # The test subjects.
-        subjects = sbj_dir_dict.keys()
+        subjects = set(sbj_dir_dict.keys())
         # The test source directories.
         inputs = sbj_dir_dict.values()
+        # Delete the existing test subjects, since staging only detects
+        # new visits.
+        with qixnat.connect() as xnat:
+            xnat.delete_subjects(project(), *subjects)
         
-    def test_staging_iterator(self):
-        dirs = glob.glob(FIXTURE + '/Subj*')
-        stg = iter_stage('Sarcoma', *dirs)
-        actual_sbjs = set([item[0] for item in stg])
-        assert_not_equal(len(actual_sbjs), 0, "No scan images were discovered")
-        assert_equal(actual_sbjs, SUBJECTS, "Subjects are incorrect: %s" %
-                                                 actual_sbjs)
-
+        
+        # Iterate over the scans.
+        discovered = list(iter_stage(collection, *inputs))
+        discovered_sbjs = set([item[0] for item in discovered])
+        assert_not_equal(len(discovered_sbjs), 0, 'No scan images were discovered')
+        assert_equal(discovered_sbjs, subjects, "Subjects are incorrect: %s" %
+                                           discovered_sbjs)
+        
+        # Return the iterated tuples for further testing.
+        return discovered    
 
 if __name__ == "__main__":
     import nose
