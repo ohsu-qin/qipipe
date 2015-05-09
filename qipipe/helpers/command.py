f86994192bb4424731aa3b8a8ba314ada76b2092
FredLoney <loneyf@ohsu.edu>
2015-05-08 18:02:12 -0700

    Add general options.

--- qipipe/helpers/command.py
+++ qipipe/helpers/command.py
@@ -1,6 +1,7 @@
 """Command qipipe command options."""
 
 import qiutil
+from qiutil.command import add_options
 
 def configure_log(**opts):
     # Configure the logger for this qixnat module and the qiutil module.
