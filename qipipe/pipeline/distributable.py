7c7be53d580c0105990fcfce30c175eee353056e
FredLoney <loneyf@ohsu.edu>
2015-07-30 09:33:39 -0700

    Compare to None.

--- qipipe/pipeline/distributable.py
+++ qipipe/pipeline/distributable.py
@@ -1,6 +1,6 @@
 from qiutil.which import which
 
-DISTRIBUTABLE = not not which('qsub')
+DISTRIBUTABLE = which('qsub') != None
 """
 Flag indicating whether the workflow can be distributed over a cluster.
 This flag is True if ``qsub`` is in the execution path, False otherwise.
