457276ba4828c1db333031b68bbffe0e31d46b15
FredLoney <loneyf@ohsu.edu>
2015-07-30 09:39:18 -0700

    Rename slice_index to slice_sequence_number.

--- qipipe/staging/roi.py
+++ qipipe/staging/roi.py
@@ -12,31 +12,33 @@ class ROIError(Exception):
 
 class LesionROI(object):
     """
-    Aggregate with attributes :attr:`lesion`, :attr:`slice` and :attr:`path`.
+    Aggregate with attributes :attr:`lesion`, :attr:`slice` and :attr:`location`.
     """
-    def __init__(self, lesion, slice_number, path):
+    def __init__(self, lesion, slice_sequence_number, location):
         """
         :param lesion: the :attr:`lesion` value
         :param slice_number: the :attr:`slice` value
-        :param path: the :attr:`path` value
+        :param location: the :attr:`location` value
         """
         self.lesion = lesion
         """The lesion number."""
         
-        self.slice = slice_number
-        """The slice number."""
+        self.slice = slice_sequence_number
+        """The one-based slice sequence number."""
         
-        self.path = path
+        self.location = location
         """The absolute BOLERO ROI .bqf file path."""
-    
+
     def __repr__(self):
-        return str(dict(lesion=self.lesion, slice=self.slice, path=self.path))
+        return (self.__class__.__name__ +
+                str(dict(lesion=self.lesion, slice=self.slice,
+                         location=self.location)))
 
 
 def iter_roi(glob, regex, input_dir):
     """
     Iterates over the the BOLERO ROI mask files in the given input directory.
-    This method is a :class:LesionROI generator, e.g.::
+    This method is a :class:`LesionROI` generator, e.g.::
 
         >>> # Find .bqf files anywhere under /path/to/session/processing.
         >>> next(iter_roi('processing/*', '.*/\.bqf', '/path/to/session'))
@@ -50,15 +52,18 @@ def iter_roi(glob, regex, input_dir):
     finder = qiutil.file.Finder(glob, regex)
     for match in finder.match(input_dir):
         # If there is no lesion qualifier, then there is only one lesion.
-        lesion_s = match.group('lesion')
+        try:
+            lesion_s = match.group('lesion')
+        except IndexError:
+            lesion_s = None
         lesion = int(lesion_s) if lesion_s else 1
         # If there is no slice index, then complain.
-        slice_index_s = match.group('slice_index')
-        if not slice_index_s:
+        slice_seq_nbr_s = match.group('slice_sequence_number')
+        if not slice_seq_nbr_s:
             raise ROIError("The BOLERO ROI slice could not be determined" +
                            " from the file path: %s" % path)
-        slice_ndx = int(slice_index_s)
+        slice_seq_nbr = int(slice_seq_nbr_s)
         # Prepend the base directory to the matching file path.
         path = os.path.join(input_dir, match.group(0))
         
-        yield LesionROI(lesion, slice_ndx, os.path.abspath(path))
+        yield LesionROI(lesion, slice_seq_nbr, os.path.abspath(path))
