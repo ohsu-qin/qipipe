bfdf65558e2f9b5b4e8d385b2911db374ffbfe03
FredLoney <loneyf@ohsu.edu>
2015-11-04 12:08:24 -0800

    Rename the filename argument to spreadsheet.

--- qipipe/qiprofile/update.py
+++ qipipe/qiprofile/update.py
@@ -4,7 +4,7 @@ from qiprofile_rest_client.model.imaging import Session
 from . import (clinical, imaging)
 
 
-def update(project, collection, subject, session, filename):
+def update(project, collection, subject, session, spreadsheet):
     """
     Updates the qiprofile database from the clinical spreadsheet and
     XNAT database for the given session.
@@ -13,12 +13,13 @@ def update(project, collection, subject, session, filename):
     :param collection: the image collection name
     :param subject: the subject number
     :param session: the XNAT session number
-    :param filename: the XLS input file location
+    :param spreadsheet: the spreadsheet input file location
+    :param modeling_technique: the modeling technique
     """
     # Get or create the subject database subject.
     key = dict(project=project, collection=collection, number=subject)
     sbj = database.get_or_create(Subject, key)
     # Update the clinical information from the XLS input.
-    clinical.update(sbj, filename)
+    clinical.update(sbj, spreadsheet)
     # Update the imaging information from XNAT.
     imaging.update(sbj, session)
