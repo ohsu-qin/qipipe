3fef7f72ebe37eb02397de2a87249a307b75171e
FredLoney <loneyf@ohsu.edu>
2015-12-07 17:52:54 -0800

    Standardize white space.

--- qipipe/interfaces/mri_volcluster.py
+++ qipipe/interfaces/mri_volcluster.py
@@ -7,33 +7,34 @@ from nipype.interfaces.traits_extension import Undefined
 
 
 class MriVolClusterInputSpec(CommandLineInputSpec):
-    in_file = traits.File(desc='Input file',
-                          mandatory=True,
-                          exists=True,
+    in_file = traits.File(desc='Input file', mandatory=True, exists=True,
                           argstr='--in %s')
-    min_thresh = traits.Float(0.0,
-                              usedefault=True,
+
+    min_thresh = traits.Float(0.0, usedefault=True,
                               desc="Minimum threshold value",
-                              argstr='--thmin %s')
+                              argstr='--thmin %s')0
+
     max_thresh = traits.Float(desc="Maximum threshold value",
                               argstr='--thmax %s')
+
     min_size = traits.Float(desc="Minimum cluster size in mm^3",
                             argstr='--minsize %s')
+
     min_voxels = traits.Int(desc="Minimum number of voxels in cluster",
                             argstr='--minsizevox %s')
-    out_clusters_name = traits.Str('clusters.nii.gz',
-                                   usedefault=True,
-                                   desc="Name out output image with cluster labels",
+
+    out_clusters_name = traits.Str('clusters.nii.gz', usedefault=True,
+                                   desc="Name of output image with cluster labels",
                                    argstr="--ocn %s")
-    out_masked_name = traits.Str('masked.nii.gz',
-                                 usedefault=True,
+
+    out_masked_name = traits.Str('masked.nii.gz', usedefault=True,
                                  desc="Name of output masked image",
                                  argstr="--out %s")
 
 
 class MriVolClusterOutputSpec(TraitedSpec):
-    out_cluster_file = traits.File(
-        desc="Output image containing cluster labels")
+    out_cluster_file = traits.File(desc="Output image containing cluster labels")
+
     out_masked_file = traits.File(desc="Input image masked by clusters")
 
 
@@ -45,7 +46,9 @@ class MriVolCluster(CommandLine):
     """
 
     input_spec = MriVolClusterInputSpec
+
     output_spec = MriVolClusterOutputSpec
+
     cmd = 'mri_volcluster'
 
     def _list_outputs(self):
