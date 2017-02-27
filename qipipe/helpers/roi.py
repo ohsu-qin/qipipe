76a3189da1c491a9ecad594c59a94972d25565dc
FredLoney <loneyf@ohsu.edu>
2017-02-27 12:23:56 -0800

    Reorder the mask.

--- qipipe/helpers/roi.py
+++ qipipe/helpers/roi.py
@@ -2,7 +2,6 @@
 import itertools
 from collections import defaultdict
 import nibabel as nib
-import numpy as np
 from scipy.spatial import ConvexHull
 from scipy.spatial.kdtree import minkowski_distance
 from mpl_toolkits.mplot3d import Axes3D
@@ -10,10 +9,32 @@ import matplotlib.pyplot as plt
 from qiutil.collections import concat
 
 
+def reorder_bolero_mask(in_file, out_file=None):
+    """
+    Since the OHSU Bolero ROI is drawn over DICOM slice
+    displays, the converted NIfTI file x and y must be
+    transposed and flipped to match the time series.
+    The mask data shape is assumed to be [x, y, slice].
+
+    :param in_file: the input Bolero mask file path
+    :param out_file: the optional output file path
+    :return: the reordered mask ndarray data
+    """
+    img = nib.load(in_file)
+    data = img.get_data()
+    transposed = data.transpose([1, 0, 2])
+    flipped = transposed[::-1, :, :]
+    if out_file:
+        out_img = nib.Nifti1Image(flipped, affine=img.affine)
+        nib.save(out_img, out_file)
+
+    return flipped
+
+
 def load(location, scale=None):
     """
     Loads a ROI mask file.
-    
+
     :param location: the ROI mask file location
     :param tuple scale: the (x, y, z) scaling factors
     :return: the :class:`ROI` encapsulation
@@ -26,14 +47,14 @@ def load(location, scale=None):
     # The non-zero points.
     non_zero = data.nonzero()
     points = np.transpose(non_zero)
-    
+
     # Return the ROI summary.
     return ROI(points, scale)
 
 
 class ROI(object):
     """Summary information for a 3D ROI mask."""
-    
+
     def __init__(self, points, scale=None):
         """
         :param points: the ROI mask points
@@ -41,7 +62,7 @@ class ROI(object):
         """
         self.extent = Extent(points, scale)
         """The 3D :class:`Extent`."""
-        
+
         sliced = defaultdict(list)
         for x, y, z in points:
             sliced[z].append((x, y))
@@ -52,7 +73,7 @@ class ROI(object):
         The {z: :class:`Extent`} dictionary for the 2D (x, y) points
         grouped by z value.
         """
-    
+
     def maximal_slice_index(self):
         """
         :return: the zero-based slice index with maximal planar extent
@@ -64,7 +85,7 @@ class ROI(object):
             if extent.area > max_area:
                 index = i
                 max_area = extent.area
-        
+
         return index
 
 
@@ -73,7 +94,7 @@ class Extent(object):
     The line segments which span the largest volume or area
     between a set of points.
     """
-    
+
     def __init__(self, points, scale=None):
         """
         :param points: the points array
@@ -102,28 +123,28 @@ class Extent(object):
         vertices = ConvexHull(points).vertices
         self.boundary = points[vertices]
         """The convex hull boundary in image space."""
-        
+
         # The boundary in anatomical space.
         scaled_bnd = scaled[vertices]
         factory = ExtentSegmentFactory(scaled_bnd)
         segment_indexes = np.asarray(factory.create())
         self.segments = self.boundary[segment_indexes]
         """The orthogonal extent segments."""
-    
+
     @property
     def area(self):
         if not self._area:
             raise ExtentError("This 3D extent has a volume rather than an"
                               " area")
         return self._area
-    
+
     @property
     def volume(self):
         if not self._volume:
             raise ExtentError("This 2D extent has an area rather than a"
                               " volume")
         return self._volume
-    
+
     def show(self):
         """Displays the ROI boundary points and extent segments."""
         # The boundary points.
@@ -138,11 +159,11 @@ class Extent(object):
         # The figure axes.
         projection = "%dd" % dim
         axes = fig.gca(projection=projection)
-        
+
         # Plot the boundary points as small clear circles.
         bnd_axes = [bnd_pts[:, i] for i in range(dim)]
         axes.scatter(*bnd_axes, c='w')
-        
+
         # Plot the segment lines.
         seg_colors = ['r', 'b', 'g']
         for i, seg in enumerate(self.segments):
@@ -152,7 +173,7 @@ class Extent(object):
             seg_axes = [seg_pts[:, j] for j in range(dim)]
             seg_color = seg_colors[i]
             axes.plot(*seg_axes, c=seg_color)
-        
+
         # Plot the bounding box faces.
         bb = np.asarray(self.bounding_box())
         if self.scale:
@@ -172,15 +193,15 @@ class Extent(object):
             face = np.asarray(face)
             face_axes = [face[:, i] for i in range(dim)]
             axes.plot(*face_axes, c='Moccasin')
-        
+
         # Display the boundary, segments and bounding box faces.
         plt.show()
-    
+
     def bounding_box(self):
         """
         Returns the (least, most) points of a rectangle
         circumscribing the extent.
-        
+
         :return: the (least, most) rectangle points
         :rtype: tuple
         """
@@ -188,7 +209,7 @@ class Extent(object):
         least = self.boundary.min(0)
         # The largest axis values.
         most = self.boundary.max(0)
-        
+
         # The bounding axes.
         return (least, most)
 
@@ -198,28 +219,28 @@ class ExtentSegmentFactory(object):
     A utility factory class that computes the extent line segments from
     a set of convex hull vertex points.
     """
-    
+
     def __init__(self, points):
         """
         :param points: the convex hull vertex points
         """
         self.points = np.asarray(points)
         """The ndarray of boundary points."""
-        
+
         self.distances = self._distances(self.points)
         """
         The N x N point distance array, where N is the number
         of points and ``self.distances[i][j]`` is the distance
         from ``self.points[i]`` to ``self.points[j]``.
         """
-    
+
     def create(self):
         """
         Returns the orthogonal segments end point indexes as the
         tuple (longest, widest, deepest), where each of the tuple
         elements is a (from, to) segment end point pair of indexes
         into the :attr:`points`, e.g.::
-            
+
             >>> points.shape
             (128, 3)
             >>> factory = ExtentSegmentFactory(points)
@@ -231,31 +252,31 @@ class ExtentSegmentFactory(object):
             True
             >>> np.all(np.equal(segments[0][1], points[12]))
             True
-        
+
         The bounding segments procedure is as follows:
-        
+
         * Find the length segment *(r1, r2)* which maximizes the
           Cartesian distance between points.
-        
+
         * Find the point *r3* furthest from *r1* and *r2*.
-        
+
         * Compute the point *o* orthogonal to *r3* on the segment
           *(r1, r2)*.
-        
+
         * The width segment is then *(r3, r4)*, where the point
           *r4* minimizes the angle between the segments *(r3, p)*
           and *(r3, o)* for all points p.
-        
+
         * Iterate on a generalization of the above algorithm to
           find the depth segment *(r5, r6)*, where:
-          
+
           - *r5* maximizes the distance to the plane formed by the
              length segment *(r1, r2)* and the width segment
              *(r3, r4)*.
-          
+
           - *r6* is the point which is most orthogonal to the length
              and width segments.
-        
+
         :return: the orthogonal segment end point index tuples
         :rtype: list
         """
@@ -265,9 +286,9 @@ class ExtentSegmentFactory(object):
         for i in range(0, dim):
             longest = self._longest_segment(*segments)
             segments.append(longest)
-        
+
         return segments
-    
+
     def _longest_segment(self, *reference):
         """
         Returns the maximal distance segment index tuple (i, j),
@@ -275,7 +296,7 @@ class ExtentSegmentFactory(object):
         reference segments, then the segments are adjusted to be
         as orthogonal to the reference segments as the point space
         allows.
-        
+
         :param reference: the segment point index tuples to
             constrain the query against
         :return: the maximal segment point index pair
@@ -284,7 +305,7 @@ class ExtentSegmentFactory(object):
         if not reference:
             flat_ndx = self.distances.argmax()
             return np.unravel_index(flat_ndx, self.distances.shape)
-        
+
         # The number of reference segments.
         ref_cnt = len(reference)
         # The point count and dimension
@@ -304,7 +325,7 @@ class ExtentSegmentFactory(object):
         ruvs = self._units(rs[:, 1], offsets)
         # The reference unit vector axes.
         ruvt = ruvs.transpose()
-        
+
         # Mask out references in the points and distances for the
         # calculations below.
         masked_pts = np.ma.asarray(self.points)
@@ -327,7 +348,7 @@ class ExtentSegmentFactory(object):
         else:
             raise NotImplementedError("More than two reference segments is"
                                       "not supported: %d" % ref_cnt)
-        
+
         # The furthest point.
         furthest = self.points[furthest_ndx]
         # The distances from the furthest point to the reference
@@ -346,7 +367,7 @@ class ExtentSegmentFactory(object):
         ovs = np.transpose(ruvt * odist)
         # The (furthest, orthogonal) unit vectors.
         ouvs = self._units(ovs, fvs)
-        
+
         # Mask out the furthest point, since we are looking
         # for a point besides the furthest point.
         masked_pts[furthest_ndx] = np.ma.masked
@@ -359,7 +380,7 @@ class ExtentSegmentFactory(object):
         # * D is the number of point dimensions (3 is supported,
         #   but 2 probably works)
         puvs = self._units(shifted, fvs)
-        
+
         # The (point, orthogonal) cosines in a R x P matrix.
         pcost = [np.dot(puvs[:, i, :], ouv) for i, ouv in enumerate(ouvs)]
         # Flip the R x P matrix into a P x R matrix.
@@ -388,7 +409,7 @@ class ExtentSegmentFactory(object):
         mask = np.repeat(amask, ref_cnt).reshape(*pcos.shape)
         # Apply the mask.
         pcos[mask] = np.ma.masked
-        
+
         # For reference vector r, furthest vector f, point vector p,
         # orthogonal intersection vector o, (f, o) angle theta and
         # (p, o) angle gamma, (f, p) is orthogonal to r if and only
@@ -405,15 +426,15 @@ class ExtentSegmentFactory(object):
         # The target segment end point opposite to the furthest point
         # minimizes the difference between the deltas.
         other_ndx = np.argmin(delta)
-        
+
         # Return the best match point indexes.
         return (furthest_ndx, other_ndx)
-    
+
     def _units(self, point, offset):
         """
         Transforms the given point array into a unit vector array
         translated to the offset.
-        
+
         :param point: an array whose last dimension holds one or
             points to transform
         :param offset: an array whose last dimension holds one or
@@ -428,18 +449,18 @@ class ExtentSegmentFactory(object):
             # Apply the unit transformation to each point,
             # preserving the shape of the input.
             return np.apply_along_axis(self._unit, pv.ndim - 1, pv)
-    
+
     def _unit(self, vector):
         """
         :return: the given vector scaled to unit length
         """
         return vector / np.linalg.norm(vector)
-    
+
     def _furthest_point(self, distances, *reference):
         """
         Returns the point furthest from the given reference point
         indexes.
-        
+
         :param distances: the N x N point distance matrix
         :param reference: the point indexes to query against
         :return: the index of the point furthest from the
@@ -452,22 +473,22 @@ class ExtentSegmentFactory(object):
         ref_dists = distances[:, reference].transpose()
         # The distance from all reference points.
         tot_dists = np.sum(ref_dists, axis=0)
-        
+
         return np.argmax(tot_dists)
-    
+
     def _distances(self, points):
         """
         Returns a N x N matrix of the (i,j) point distances, where:
         * N = the number of points
         * i, j = 0, ..., N
-        
+
         :param points: the subject points
         :return: the distance array
         :rtype ndarray
         """
         dists = [[distance(p, q) for p in self.points]
                  for q in self.points]
-        
+
         return np.asarray(dists)
 
 
