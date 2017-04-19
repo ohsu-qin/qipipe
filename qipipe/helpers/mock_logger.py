f653afef4ff5d89f40b08714abefae1ce11916e5
FredLoney <loneyf@ohsu.edu>
2017-04-19 10:47:52 -0700

    Flush stdout.

--- qipipe/helpers/mock_logger.py
+++ qipipe/helpers/mock_logger.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import datetime
 import logging
 
@@ -7,13 +8,15 @@ class MockLogger(object):
     This MockLogger prints all log messages to stdout.
     This works around the following Nipype bug:
 
-    * Nipype stomps on any other application logging. The work-around
-      is to mock a "logger" that writes to stdout.
+    * Nipype stomps on any other application's logging.
+      The work-around is to mock a "logger" that writes
+      to stdout.
 
-    The log message is preceded by the process id.
+    The log message is preceded by the date, time and process id.
 
     Note: Log messages might be interleaved from different nodes
-    in a cluster environment.
+    in a cluster environment. The prefix process id helps
+    disambiguate the log context.
     """
     def __init__(self, level=None):
         if not level:
@@ -21,23 +24,25 @@ class MockLogger(object):
         self.level_s = level
         self.level = getattr(logging, self.level_s)
         self.pid = os.getpid()
+        self.info('Mock logger enabled.')
 
     def info(self, message):
         if self.level <= logging.INFO:
-            print "%s %s" % (self.prefix(), message)
+            self._write(message)
 
     def error(self, message):
         if self.level <= logging.ERROR:
-            print "%s %s" % (self.prefix(), message)
+            self._write(message)
 
     def warn(self, message):
         if self.level <= logging.WARN:
-            print "%s %s" % (self.prefix(), message)
+            self._write(message)
 
     def debug(self, message):
         if self.level <= logging.DEBUG:
-            print "%s %s" % (self.prefix(), message)
+            self._write(message)
 
-    def prefix(self):
+    def _write(self, message):
         dt = datetime.now().strftime("%M/%D/%Y %H:%M:%S")
-        return "%s (%s) %s" % (dt, self.pid, self.level_s)
+        print "%s (%d) %s %s" % (dt, self.pid, self.level_s, message)
+        sys.stdout.flush()
