635bf1a5627cdb5fbe7ec5f67994cb97aac4deef
FredLoney <loneyf@ohsu.edu>
2017-05-16 12:35:40 -0700

    Use the reorder_bolero_mask helper directly.

--- qipipe/interfaces/reorder_bolero_mask.py
+++ qipipe/interfaces/reorder_bolero_mask.py
@@ -3,14 +3,16 @@ This module reorders the OHSU AIRC ``bolero_mask_conv``
 result to conform with the time series x and y order.
 """
 import os
-from nipype.interfaces.base import (traits, BaseInterface, TraitedSpec)
+from nipype.interfaces.base import (
+    traits, isdefined, BaseInterfaceInputSpec, BaseInterface, TraitedSpec
+)
 from qiutil.file import splitexts
+from ..helpers import roi
 
 
-class ReorderBoleroMaskInputSpec(CommandLineInputSpec):
-    in_file = traits.Str(desc='Input mask file name', mandatory=True,
-                         position=1, argstr='\"%s\"')
-    out_file = traits.Str(desc='Output file name', argstr='-o %s')
+class ReorderBoleroMaskInputSpec(BaseInterfaceInputSpec):
+    in_file = traits.Str(desc='Input mask file name', mandatory=True)
+    out_file = traits.Str(desc='Output file name')
 
 
 class ReorderBoleroMaskOutputSpec(TraitedSpec):
@@ -19,42 +21,29 @@ class ReorderBoleroMaskOutputSpec(TraitedSpec):
 
 class ReorderBoleroMask(BaseInterface):
     """
-    Interface to the mask reordering utility.
+    Interface to the ROI reordering utility.
     """
 
-    _cmd = 'reorder_bolero_mask'
     input_spec = ReorderBoleroMaskInputSpec
+
     output_spec = ReorderBoleroMaskOutputSpec
 
-    def __init__(self, **inputs):
-        super(ReorderBoleroMask, self).__init__(**inputs)
-
-
     def _run_interface(self, runtime):
-        self._out_file = self._copy(self.inputs.in_file, self.inputs.dest,
-                                    self.inputs.out_fname)
+        in_file = self.inputs.in_file
+        # Make the default output file path, if necessary.
+        if isdefined(self.inputs.out_file):
+            out_file = self.inputs.out_file
+        else:
+            def_out_file = self._default_output_file_name(in_file)
+            out_file = self.inputs.out_file = def_out_file
 
-    def _run_interface(self, **inputs):
-        if not inputs.get('out_file'):
-            in_file = inputs.get('in_file')
-            # in_file is mandatory, but let the superclass run
-            # method check that and throw the appropriate error.
-            if in_file:
-                def_out_file = self._default_output_file_name(in_file)
-                inputs['out_file'] = self.inputs.out_file = def_out_file
-
-
-        print (">>rbm inputs: %s" % inputs)
-        print (">>rbm self.inputs.out_file: %s" % self.inputs.out_file)
+        roi.reorder_bolero_mask(in_file, out_file=out_file)
 
         return runtime
 
-
-        super(ReorderBoleroMask, self).run(**inputs)
-
     def _list_outputs(self):
         outputs = self._outputs().get()
-        # Expand the output path, if necessary.
+        # Capture the expanded output path.
         outputs['out_file'] = os.path.abspath(self.inputs.out_file)
 
         return outputs
