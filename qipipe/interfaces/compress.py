c76041deec07b5ed31ad0276887dce7483d4739e
FredLoney <loneyf@ohsu.edu>
2017-06-19 15:03:22 -0700

    Streamline open with contexts.

--- qipipe/interfaces/compress.py
+++ qipipe/interfaces/compress.py
@@ -7,8 +7,8 @@ from nipype.interfaces.base import (BaseInterfaceInputSpec, TraitedSpec,
 class CompressInputSpec(BaseInterfaceInputSpec):
     in_file = File(exists=True, mandatory=True, desc='The file to compress')
 
-    dest = Directory(desc='The optional directory to write the compressed file'
-                     '(default current directory)')
+    dest = Directory(desc='The optional directory to write the compressed'
+                          ' file (default current directory)')
 
 
 class CompressOutputSpec(TraitedSpec):
@@ -21,8 +21,8 @@ class Compress(BaseInterface):
     output_spec = CompressOutputSpec
 
     def _run_interface(self, runtime):
-        self.out_file = self._compress(
-            self.inputs.in_file, dest=self.inputs.dest)
+        self.out_file = self._compress(self.inputs.in_file,
+                                      dest=self.inputs.dest)
 
         return runtime
 
@@ -40,16 +40,15 @@ class Compress(BaseInterface):
         :param dest: the destination (default is the working directory)
         :return: the compressed file path
         """
-        if not dest:
+        if dest:
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+        else:
             dest = os.getcwd()
-        if not os.path.exists(dest):
-            os.makedirs(dest)
         _, base_name = os.path.split(in_file)
         out_file = os.path.join(dest, base_name + '.gz')
-        f = open(in_file, 'rb')
-        cf = gzip.open(out_file, 'wb')
-        cf.writelines(f)
-        f.close()
-        cf.close()
+        with open(in_file, 'rb') as f:
+            with gzip.open(out_file, 'wb') as z:
+                z.writelines(f)
 
         return os.path.abspath(out_file)
